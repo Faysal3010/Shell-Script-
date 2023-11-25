d4a8ea755330e51022082c60d5c35e47dac7a7f1
Faysal Mahmud <95643831+Faysal3010@users.noreply.github.com>
2023-11-25 23:23:53 +0600

    Create How to make Shell Scripts file or .sh

---
+++ How to make Shell Scripts file or .sh
@@ -0,0 +1,7 @@
+Step 1: Open linux Kernel operating System or in window open gitbash
+Step 2: now make file in terminal type:  touch file.sh or you can directly type nano file.sh
+Step 3: then press enter and get into bash shell and type your script
+Step 4: After complete you script then press ctrl+x (save)then y(yes)/n(no) then enter
+Step 5: If you save the file and enter you move automatically to terminate and type ./file.sh and see you script 
+Step 6: if compuetr want permission type chmod +x file.sh.
+
